f03c73eb36d741bfb2c07f9204023dda9f56dd74
bxnnyboi <malewski.draws@gmail.com>
2023-03-23 11:16:52 +0100

    Added Projectile 1 prefab and edited Projectile1 script

--- Tower-Defense/Assets/Scripts/Projectile/Projectile1.cs
+++ Tower-Defense/Assets/Scripts/Projectile/Projectile1.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Projectile1 : MonoBehaviour
 {
+    public float projectileSpeed = 500;
+
     // Start is called before the first frame update
     void Start()
     {
