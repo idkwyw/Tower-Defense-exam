95dbf83b50d73470d5a0d49464483cd8de44b2d9
bxnnyboi <malewski.draws@gmail.com>
2023-03-24 10:41:44 +0100

    Updated Enemy1 script

--- Tower-Defense/Assets/Scripts/Enemies/Enemy1.cs
+++ Tower-Defense/Assets/Scripts/Enemies/Enemy1.cs
@@ -5,15 +5,4 @@ using UnityEngine;
 public class Enemy1 : MonoBehaviour
 {
     public int Health = 100;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
